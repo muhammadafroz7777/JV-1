cfefea4c1a2f55b34981859e137aea3d5d103cf5
muhammadafroz7777 <muhammadafroz0320@gmail.com>
2024-02-23 04:58:33 +0500

    con

---
+++ app.js
@@ -0,0 +1,11 @@
+// alert("Hi Afo");
+
+// console.log("hello World");
+
+// var $_userName123="Muhammad";
+// console.log("$_userName");
+
+var firstName"Muhammad"
+var lastName"Afroz"
+
+console.log(firstName + " " + lastName);
