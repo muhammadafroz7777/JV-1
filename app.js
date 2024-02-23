021a5ee6aea47056e481e39f8c9496d55159eb3e
muhammadafroz7777 <muhammadafroz0320@gmail.com>
2024-02-23 10:41:11 +0500

    second class topic (Comparigan)

--- app.js
+++ app.js
@@ -5,7 +5,28 @@
 // var $_userName123="Muhammad";
 // console.log("$_userName");
 
-var firstName"Muhammad"
-var lastName"Afroz"
+// var firstName"Muhammad"
+// var lastName"Afroz"
 
-console.log(firstName + " " + lastName);
+// console.log(firstName + " " + lastName);
+
+
+
+// var num1 =5;
+// var num2 =5;
+
+// var num3 = "af0";
+
+// console.log(num1 + num2 + num3);
+
+// console = (typeof(num3))
+
+// var num1 = +prompt('Enter First Num');
+// var num2 = +prompt('Enter second Num');
+ 
+
+// console.log(num1 > num2);
+
+console.log(10 == 10);
+
+
